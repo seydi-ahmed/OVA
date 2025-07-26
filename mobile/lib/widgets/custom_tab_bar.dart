b17a1d18f9ffa9ae5357e2e04bb6f4ba30e033ae
oumy25 <kaneoumylaye@gmail.com>
2025-07-26 00:59:31 +0000

    add menu and page

---
+++ mobile/lib/widgets/custom_tab_bar.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import '../screens/home/home_page.dart';
+
+class CustomTabBar extends StatefulWidget {
+  const CustomTabBar({super.key});
+
+  @override
+  State<CustomTabBar> createState() => _CustomTabBarState();
+}
+
+class _CustomTabBarState extends State<CustomTabBar> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 5, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: TabBarView(
+        controller: _tabController,
+        children: const [
+          HomePage(),
+          Center(child: Text('Recherche')),
+          Center(child: Text('Statistiques')),
+          Center(child: Text('Profil')),
+          Center(child: Text('Messages')),
+        ],
+      ),
+      bottomNavigationBar: Material(
+        // color: Theme.of(context).primaryColor,
+        child: TabBar(
+          controller: _tabController,
+          tabs: const [
+            Tab(icon: Icon(Icons.home)),
+            Tab(icon: Icon(Icons.grid_view_outlined)),
+            Tab(icon: Icon(Icons.show_chart)),
+            Tab(icon: Icon(Icons.person_3)),
+            Tab(icon: Icon(Icons.chat)),
+          ],
+          labelColor: Color(0xFF000000),
+          unselectedLabelColor: Color(0xFF4D4D4D),
+          indicatorColor: Colors.transparent,
+        ),
+      ),
+    );
+  }
+}
